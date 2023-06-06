29c62718b8b3e47f6a92f747963e57ce345aa257
Suhail-007 <mohdsuhail51998@gmail.com>
2023-06-06 13:40:15 +0530

    remove a duplicate css file rendering nextjs image instead of regular img from MD

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,18 +1,40 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 
 import PostHeader from './post-header';
 
 import styles from './post-content.module.css';
 
-function PostContent({slug, image, content, title}) {
+function PostContent({ slug, image, content, title }) {
     const imagePath = `/images/posts/${slug}/${image}`;
+
+    const customComponents = {
+        p({ node }) {
+            const element = node.children[0];
+
+            if (element.tagName !== 'img') return <p>{element.value}</p>
+
+            const { src, alt } = element.properties;
+
+            return (
+
+                <div>
+                    <Image
+                        className={styles.image}
+                        src={`/images/posts/${slug}/${src}`}
+                        alt={alt}
+                        width={600}
+                        height={300} />
+                </div>
+            )
+        },
+    }
 
     return (
         <article className={styles.content}>
             <PostHeader title={title} image={imagePath} />
 
-
-            <ReactMarkdown>
+            <ReactMarkdown components={customComponents}>
                 {content}
             </ReactMarkdown>
         </article>
