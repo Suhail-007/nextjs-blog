1bdf04a1875050536ea606cfedfe22b86b86c94e
Suhail-007 <mohdsuhail51998@gmail.com>
2023-06-09 10:22:44 +0530

    added mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,9 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
-   
+
         if (
             !email ||
             !email.includes('@') ||
@@ -20,7 +22,26 @@ export default function handler(req, res) {
             message
         }
 
-        console.log(newMessage);
+        let client;
+
+        try {
+            client = await MongoClient.connect('mongodb+srv://suhail-007:Suhail1998@cluster0.y6s0gvs.mongodb.net/myblog?retryWrites=true&w=majority');
+        } catch (error) {
+            res.status(500).json({ message: 'Could not connect to Database ', error: error });
+        }
+
+        const db = client.db();
+
+        try {
+            const result = await db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close();
+            res.json(500).json({ message: 'Storing message failed', error: error });
+            return
+        }
+
+        client.close()
         res.status(200).json({ message: 'Successfully stored message!', data: newMessage })
     }
 
